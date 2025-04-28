8e095db41081b56571b63f4d4ff26143255b1ddf
jang-jorim <yrimjang1224@gmail.com>
2025-04-28 16:51:07 +0900

    gradle 멀티모듈 추가  - api, business-api, admin, batch, base

---
+++ business-api/src/test/java/com/example/businessapi/BusinessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
