f9ec3ce111477818dc6f956c2956dfcfd3b30e60
jang-jorim <yrimjang1224@gmail.com>
2025-04-28 16:19:09 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/example/stayroom/StayroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stayroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayroomApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
